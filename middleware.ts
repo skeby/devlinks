9a7165125a23a0533fb686a775a08728ada193e4
Adeyinka Akinsanya <akinsanyaadeyinka4166@gmail.com>
2025-08-22 01:59:41 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -67,5 +67,10 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/((?!_next|api|.*\\.).*)", "/api/login", "/api/logout"],
+  matcher: [
+    "/",
+    "/((?!api|_next/static|_next/image|favicon.ico|u/.*).*)",
+    "/api/login",
+    "/api/logout",
+  ],
 };
